210d100017f85d13510d7a2eb7a328c7252baf5b
caohou98 <caohou98@gmail.com>
2025-11-21 19:22:45 +0800

    下单接口

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    allowedHosts: [
+      'testapi.g123.top', // 添加你想允许的主机名
+      'localhost',         // 如果需要，可以同时允许 localhost
+      '127.0.0.1'          // 如果需要，也可以加入本地 IP 地址
+    ]
   }
+  
 })
 
